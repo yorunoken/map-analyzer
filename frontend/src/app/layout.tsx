12b4579b7fef6014e22aa62ba29ed1c30ac75a6b
Yoru <m.f.fetvaci@hotmail.com>
2024-10-04 23:57:28 +0300

    trying smth out again

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,9 +4,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import Script from "next/script";
 import { cn } from "@/lib/utils";
-import { Head } from "next/document";
 
 export const metadata: Metadata = {
     title: "osu! beatmap analyzer",
@@ -20,12 +18,13 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <Head>
-                <Script
+            <head>
+                <script
+                    defer
                     src="https://cloud.umami.is/script.js"
                     data-website-id="b95e60a2-630f-4dab-814f-7299ebab3d61"
-                />
-            </Head>
+                ></script>
+            </head>
             <body
                 className={cn(
                     "min-h-screen bg-background font-sans antialiased",

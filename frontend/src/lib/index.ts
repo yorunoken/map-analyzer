8873167c6b1e3d4d918d38531fd08101075acd79
Yoru <m.f.fetvaci@hotmail.com>
2024-10-04 22:36:54 +0300

    have dev thing

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL = `http://127.0.0.1:${process.env.BACKEND_PORT}`;
+export const BACKEND_URL = `http://${process.env.DEV ? "127.0.0.1" : "localhost"}:${process.env.BACKEND_PORT}`;

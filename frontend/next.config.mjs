535c37be34998dba94266bc4c64ae9dd14df31a1
Yoru <m.f.fetvaci@hotmail.com>
2024-10-04 22:59:50 +0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     experimental: {
         serverActions: {
-            allowedOrigins: ["localhost"],
+            allowedOrigins: ["analyze.yorunoken.com", "127.0.0.1:3006"],
         },
     },
     images: {

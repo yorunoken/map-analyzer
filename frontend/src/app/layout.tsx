9c3849b3e47cdce570a3122f2976f0fe033eb155
Yoru <m.f.fetvaci@hotmail.com>
2024-10-04 23:48:22 +0300

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -19,13 +19,6 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <head>
-                <Script
-                    src="https://cloud.umami.is/script.js"
-                    data-website-id="b95e60a2-630f-4dab-814f-7299ebab3d61"
-                    strategy="beforeInteractive"
-                />
-            </head>
             <body
                 className={cn(
                     "min-h-screen bg-background font-sans antialiased",
@@ -45,6 +38,11 @@ export default function RootLayout({
                     <Toaster />
                 </ThemeProvider>
             </body>
+            <Script
+                src="https://cloud.umami.is/script.js"
+                data-website-id="b95e60a2-630f-4dab-814f-7299ebab3d61"
+                async
+            />
         </html>
     );
 }

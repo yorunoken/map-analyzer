77d97079722fc4879ef330e7ccdf9da7debcc249
Yoru <m.f.fetvaci@hotmail.com>
2024-10-04 23:54:42 +0300

    i think it will work now.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -37,12 +37,12 @@ export default function RootLayout({
                     </div>
                     <Toaster />
                 </ThemeProvider>
+                <Script
+                    src="https://cloud.umami.is/script.js"
+                    data-website-id="b95e60a2-630f-4dab-814f-7299ebab3d61"
+                    strategy="afterInteractive"
+                />
             </body>
-            <Script
-                src="https://cloud.umami.is/script.js"
-                data-website-id="b95e60a2-630f-4dab-814f-7299ebab3d61"
-                async
-            />
         </html>
     );
 }
